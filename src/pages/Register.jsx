42d6533a10cb3ecd8bc5d69a964dd4779c5d77f8
dhyey0 <dhyeypatel2907@gmail.com>
2025-03-20 12:18:53 +0530

    api call bug fix in registration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserPlus } from 'lucide-react';
-import { authService } from '../services/api';
+import { authService } from '../api';
 
 const Register = () => {
   const navigate = useNavigate();
